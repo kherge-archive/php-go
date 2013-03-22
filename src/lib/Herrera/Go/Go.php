c44beabaa0dd068f82b6959a02c3fc9e79eb1179
Kevin Herrera <kevin@herrera.io>
2013-03-22 16:31:15 -0700

    Make the Go Phar upgradeable.

--- src/lib/Herrera/Go/Go.php
+++ src/lib/Herrera/Go/Go.php
@@ -3,9 +3,15 @@
 namespace Herrera\Go;
 
 use Herrera\Cli\Application;
+use Herrera\Service\Process\ProcessServiceProvider;
+use Herrera\Service\Update\UpdateServiceProvider;
 use InvalidArgumentException;
+use Symfony\Component\Console\Application as Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Creates commands from tasks listed in a Pakefile.
@@ -28,6 +34,63 @@ class Go extends Application
         return $this->add($name, $callback)
                     ->setDescription($description)
                     ->setHelp($description);
+    }
+
+    /**
+     * Creates a new, configured instance of Go.
+     *
+     * @param string $name    The application name.
+     * @param string $version The application version.
+     *
+     * @return Go The new instance.
+     */
+    public static function create($name = 'Go', $version = '@git_version@')
+    {
+        $go = new self($name, $version);
+        $go->register(new ProcessServiceProvider());
+
+        /** @var $console Console */
+        $console = $go['console'];
+
+        if (('@' . 'git_version@') !== $console->getVersion()) {
+            $go->register(new UpdateServiceProvider(), array(
+                'update.url' => '@manifest_url@'
+            ));
+
+            /** @var $update Command */
+            $update = $go('update', 'Updates the application.', function (
+                InputInterface $input,
+                OutputInterface $output
+            ) use ($go) {
+                $output->writeln('Looking for updates...');
+
+                if ($go['update'](
+                    $go['console']->getVersion(),
+                    (false === $input->getOption('upgrade')),
+                    $input->getOption('pre')
+                )){
+                    $output->writeln('<info>Update successful!</info>');
+                } else {
+                    $output->writeln('<comment>Already up-to-date.</comment>');
+                }
+            });
+
+            $update->addOption(
+                'pre',
+                'p',
+                InputOption::VALUE_NONE,
+                'Allow pre-release updates.'
+            );
+
+            $update->addOption(
+                'upgrade',
+                'u',
+                InputOption::VALUE_NONE,
+                'Upgrade to next major release, if available.'
+            );
+        }
+
+        return $go;
     }
 
     /**
--- src/tests/Herrera/Go/Tests/GoTest.php
+++ src/tests/Herrera/Go/Tests/GoTest.php
@@ -4,6 +4,7 @@ namespace Herrera\Go\Tests;
 
 use Herrera\Go\Go;
 use Herrera\PHPUnit\TestCase;
+use Phar;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 
@@ -30,6 +31,13 @@ class GoTest extends TestCase
         $command->run(new ArrayInput(array('test')), new NullOutput());
 
         $this->assertTrue($ran);
+    }
+
+    public function testCreate()
+    {
+        $go = Go::create();
+
+        $this->assertInstanceOf('Herrera\\Go\\Go', $go);
     }
 
     /**
@@ -83,5 +91,66 @@ PAKEFILE
             'Symfony\\Component\\Console\\Command\\Command',
             $go['console']->find('test')
         );
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testUpdate()
+    {
+        $manifest = $this->createFile();
+
+        unlink($current = $this->createFile('go.phar'));
+        unlink($update = $this->createFile('go.phar'));
+
+        $phar = new Phar($current);
+        $phar->addFromString('test.php', '<?php echo 1;');
+        $phar->setStub($phar->createDefaultStub('test.php'));
+
+        unset($phar);
+
+        $phar = new Phar($update);
+        $phar->addFromString('test.php', '<?php echo 2;');
+        $phar->setStub($phar->createDefaultStub('test.php'));
+
+        unset($phar);
+
+        file_put_contents($manifest, json_encode(array(
+            array(
+                'name' => 'go.phar',
+                'sha1' => sha1_file($update),
+                'url' => "file://$update",
+                'version' => '1.0.0-alpha.1'
+            ),
+            array(
+                'name' => 'go.phar',
+                'sha1' => sha1_file($update),
+                'url' => "file://$update",
+                'version' => '2.0.0-alpha.1'
+            )
+        )));
+
+        $_SERVER['argv'][0] = $current;
+
+        $go = Go::create('Test', '1.0.0');
+        $go['update.url'] = "file://$manifest";
+        $go['console']->setAutoExit(false);
+        $go['console']->run(
+            new ArrayInput(array('command' => 'update')),
+            new NullOutput()
+        );
+
+        $this->assertEquals('1', exec('php ' . escapeshellarg($current)));
+
+        $go['console']->run(
+            new ArrayInput(array(
+                'command' => 'update',
+                '--upgrade' => true,
+                '--pre' => true
+            )),
+            new NullOutput()
+        );
+
+        $this->assertEquals('2', exec('php ' . escapeshellarg($current)));
     }
 }
